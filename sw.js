93feafa69f2d22e4908d62c5bf12b64cea6bcc49
nahidsaikat <nahidsaikatft40@gmail.com>
2019-06-15 21:13:04 +0600

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-962ac77f98bf69eefa31.js"
+    "url": "webpack-runtime-6dd8e78b64c438f87cb7.js"
   },
   {
     "url": "styles.4e661700a8311d477e79.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ba3ac816b589a212627520e03525495a"
+    "revision": "dc6db84653a5518a66a78587e5da42d6"
   },
   {
     "url": "manifest.json",
