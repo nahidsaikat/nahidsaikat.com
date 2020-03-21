fc2f28c20bca5d1e48769304724e942c11a92874
nahidsaikat <nahidsaikatft40@gmail.com>
2020-03-21 23:46:11 +0600

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8d5184f546fbe41c6072.js"
+    "url": "webpack-runtime-e9f7f1c4d711bc4a3f8e.js"
   },
   {
     "url": "styles.4e661700a8311d477e79.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0b79184e9a52c9dc9fc6e8dfdaaada8f"
+    "revision": "d9a677f654ce6c2e24766e4188a21e48"
   },
   {
     "url": "manifest.json",
